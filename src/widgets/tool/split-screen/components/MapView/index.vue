bc0b37752970ae873f47257df9a76a5229cbe18a
gongyuejian <gongyuejian.000@mapgis.com>
2024-08-02 03:16:32 +0000

    fix(5910):PTSYB-分屏无法显示外来的OSM服务
    (cherry picked from commit 4ff582c347b8472f0ca84f82867bc2159dd4360b)
    
    (cherry picked from commit a953a12bf3c6d443c8ee484816f010539e85f805)

--- src/widgets/tool/split-screen/components/MapView/index.vue
+++ src/widgets/tool/split-screen/components/MapView/index.vue
@@ -62,7 +62,6 @@ import { Rectangle } from '@mapgis/webclient-es6-service/common/Rectangle'
 import {
   Document,
   Layer,
-  LayerType,
   Layer3D,
   Rectangle3D,
   Objects,
@@ -158,10 +157,7 @@ export default {
   computed: {
     // 是否是二维图层
     is2dLayer() {
-      return (
-        !(this.mapViewLayer instanceof Layer3D) &&
-        this.mapViewLayer.type !== LayerType.OSM
-      )
+      return !(this.mapViewLayer instanceof Layer3D)
     },
 
     // 二维或三维地图组件
