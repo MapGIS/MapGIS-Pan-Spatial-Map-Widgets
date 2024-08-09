f2251161b3547d66151c3ecc55992dc27c4077ed
gongyuejian <gongyuejian.000@mapgis.com>
2024-08-09 06:32:48 +0000

    fix(5938):PTSYB-综合查询坐标定位无法鼠标拾取坐标、无法定位
    (cherry picked from commit 9dffc98f89f65de2311ddccbf83459fef83a383a)
    
    (cherry picked from commit 4a2a963fa2122056198328e76644bdd9ec275fa7)

--- src/widgets/query/comprehensive-query/components/Coordinate/CoordinateCesium.vue
+++ src/widgets/query/comprehensive-query/components/Coordinate/CoordinateCesium.vue
@@ -82,7 +82,8 @@ export default {
         if (!this.pickable) return
         // 判断是否点击到标注点，点击到标注点则不进行拾取
         const pickedFeature = this.viewer.scene.pick(movement.position)
-        if (pickedFeature && pickedFeature.id.markLabel) return
+        if (pickedFeature && pickedFeature.id && pickedFeature.id.markLabel)
+          return
         // 通过指定的椭球或者地图对应的坐标系，将鼠标的二维坐标转换为对应椭球体三维坐标
         const { ellipsoid } = this.viewer.scene.globe
         const cartesian = this.viewer.camera.pickEllipsoid(
