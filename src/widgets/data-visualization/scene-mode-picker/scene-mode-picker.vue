6a76942b7a1d87aaf60a306111dcdfbcd44e50b5
gongyuejian <gongyuejian.000@mapgis.com>
2024-08-21 07:45:59 +0000

    fix(6012):PTSYB-场景模式切换-三维模式球形切平面位置偏移
    (cherry picked from commit acc18577a93c2310c4a1784d63ffe936803b8790)
    
    (cherry picked from commit 412dc648c9961d87ed06b29cb44f2fb99eae1bcb)

--- src/widgets/data-visualization/scene-mode-picker/scene-mode-picker.vue
+++ src/widgets/data-visualization/scene-mode-picker/scene-mode-picker.vue
@@ -11,22 +11,93 @@
 </template>
 
 <script lang="ts">
-import { WidgetMixin } from '@mapgis/web-app-framework'
+import { WidgetMixin, Objects } from '@mapgis/web-app-framework'
 
 export default {
   name: 'MpSceneModePicker',
   mixins: [WidgetMixin],
-  computed: {},
   mounted() {
     const { viewer, Cesium } = this
-    const sceneModePicker = new Cesium.SceneModePicker(
+    this.sceneModePicker = new Cesium.SceneModePicker(
       'sceneModePickerContainer',
       viewer.scene
     )
-    sceneModePicker.viewModel.duration = 0 // 去掉切换动画效果，保持视图位置不变
+    this.sceneModePicker.viewModel.duration = 0 // 去掉切换动画效果，保持视图位置不变
+
+    this.sceneController = Objects.SceneController.getInstance(
+      this.Cesium,
+      this.vueCesium,
+      this.viewer
+    )
+
+    // 监听切换成3D视图之前的事件
+    this.sceneModePicker.viewModel.morphTo3D.beforeExecute.addEventListener(
+      () => {
+        this.getCurrentCenterAndHeight()
+      }
+    )
+    // 监听切换成3D视图之后的事件
+    this.sceneModePicker.viewModel.morphTo3D.afterExecute.addEventListener(
+      () => {
+        this.resetViewer()
+      }
+    )
+
+    // 监听切换成2D视图之前的事件
+    this.sceneModePicker.viewModel.morphTo2D.beforeExecute.addEventListener(
+      () => {
+        this.getCurrentCenterAndHeight()
+      }
+    )
+    // 监听切换成2D视图之后的事件
+    this.sceneModePicker.viewModel.morphTo2D.afterExecute.addEventListener(
+      () => {
+        this.resetViewer()
+      }
+    )
+
+    // 监听切换成哥伦比亚视图之前的事件
+    this.sceneModePicker.viewModel.morphToColumbusView.beforeExecute.addEventListener(
+      () => {
+        this.getCurrentCenterAndHeight()
+      }
+    )
+    // 监听切换成哥伦比亚视图之后的事件
+    this.sceneModePicker.viewModel.morphToColumbusView.afterExecute.addEventListener(
+      () => {
+        this.resetViewer()
+      }
+    )
   },
 
-  methods: {},
+  methods: {
+    /**
+     * 获取当前视图中心点和相机高度
+     */
+    getCurrentCenterAndHeight() {
+      const extent = this.sceneController.getCurrentExtent()
+      const { xmin, ymin, xmax, ymax, height } = extent
+
+      this.center = {
+        lng: (xmin + xmax) / 2,
+        lat: (ymin + ymax) / 2,
+      }
+      this.height = height
+    },
+    // 重置视图
+    resetViewer() {
+      if (this.center) {
+        const { lng, lat } = this.center
+        this.viewer.camera.setView({
+          destination: this.Cesium.Cartesian3.fromDegrees(
+            lng,
+            lat,
+            this.height
+          ),
+        })
+      }
+    },
+  },
 }
 </script>
 
