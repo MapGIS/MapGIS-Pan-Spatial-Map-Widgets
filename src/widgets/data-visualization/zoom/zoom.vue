fba1850379f34c0eadea6c20fb295b011cf46f92
zhouzhenxiong <zhouzhenxiong.zzx@mapgis.com>
2025-11-03 15:39:33 +0800

    fix(28885): 【应用搭建】这边设置了，没有保存按钮，只有点右上角的保存，不保存也行，应该直接就同步了 问题原因:在应用搭建中左侧的‘配置’中进行设置的属性只同步了全局配置appConfig对象，未同步修改baseConfigInstance对象，微件中获取配置信息通常使用baseConfigInstance对象，当前只在进入已保存的应用时，baseConfigInstance对象中的配置才会与全局配置appConfig对象中的基础配置baseConfig对象进行同步 修改说明:当应用搭建中左侧的‘配置’中进行设置的属性变化时，同步修改全局配置appConfig对象和baseConfigInstance对象，保证微件能获取到最新的配置 接口变动:无 接口描述:无 同步版本:dev/10.7.6.10 统查统改:是，统改1处，修订应用搭建左侧配置中设置‘初始位置方式’为底图范围时，缩放微件中以底图范围进行缩放时，若处于应用搭建模式下直接从appConfig对象中获取底图管理微件配置中的索引底图

--- src/widgets/data-visualization/zoom/zoom.vue
+++ src/widgets/data-visualization/zoom/zoom.vue
@@ -163,7 +163,7 @@ export default {
            * 修改人：龚跃健
            * 修改日期：2021/12/23
            */
-          const config = await api.getWidgetConfig('basemap-manager')
+          const config = await this.getBasemapManagerWidgetConfig()
           // 获取地图底图
           const baseMap = config.baseMapList.find(
             (item) => item.guid === config.indexBaseMapGUID
@@ -185,6 +185,52 @@ export default {
           }
           break
       }
+    },
+    async getBasemapManagerWidgetConfig() {
+      let config
+      // 如果是应用搭建模式或者应用搭建预览模式直接从application对象上获取底图管理微件的配置
+      if (this.designTime || this.previewTime) {
+        const { contentWidgets, mapWidgets } = this.application
+        // 从contentWidgets中查找
+        contentWidgets.groups.forEach((group) => {
+          group.widgets.forEach((widget) => {
+            // 通过微件的uri查找底图管理微件的配置
+            const widgetConfig = this.getTargetWidgetConfig(
+              widget,
+              'basemap-manager'
+            )
+            if (widgetConfig) {
+              config = widgetConfig
+            }
+          })
+        })
+
+        // contentWidgets中未找到再去mapWidgets中查找
+        if (!config) {
+          mapWidgets.widgets.forEach((widget) => {
+            const widgetConfig = this.getTargetWidgetConfig(
+              widget,
+              'basemap-manager'
+            )
+            if (widgetConfig) {
+              config = widgetConfig
+            }
+          })
+        }
+      } else {
+        config = await api.getWidgetConfig('basemap-manager')
+      }
+      return Promise.resolve(config)
+    },
+    getTargetWidgetConfig(widget, targetWidgetName) {
+      if (widget.uri) {
+        const strs = widget.uri.split('/')
+        const widgetName = strs[strs.length - 1]
+        if (widgetName === targetWidgetName) {
+          return JSON.parse(JSON.stringify(widget.config))
+        }
+      }
+      return null
     },
 
     initFitBound(bound, mapParams) {
